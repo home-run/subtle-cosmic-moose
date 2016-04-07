7c6ef4eef843a9e59888b87275731ba38f8cb640
ozyx <jessemazzella@gmail.com>
2016-04-06 21:02:04 -0700

    [#21] Add test for opening database and some comments.

--- src/source/database.cpp
+++ src/source/database.cpp
@@ -3,20 +3,25 @@
 /*!
  * \brief Database::Database
  * \param path Path to SQL Database file
+ * \param driver QString identifier for the specific flavor of SQL we are using.
  */
-Database::Database(QString path, QString driver="QSQLITE") : QSqlDatabase(addDatabase(driver))
+Database::Database(QString path, QString driver) : QSqlDatabase(addDatabase(driver))
 {
     setHostName("localhost");
     setDatabaseName(path);
 
+    // If the database successfully opens
     if(open())
     {
         qDebug() << "Database opened successfully";
     }
     else
     {
+        // Output the last error message from the database
         qDebug() << this->lastError().text();
     }
 
+    //  We may or may not need this later on.
+    //  This is for activating foreign key support.
     //  query.exec("PRAGMA foreign_keys = ON;");
 }
