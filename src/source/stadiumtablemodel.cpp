3eb13870d258c2b7b64314ed56b1d909991d32e8
ozyx <jessemazzella@gmail.com>
2016-04-17 13:43:08 -0700

    [#7] add a few more comments.

--- src/source/stadiumtablemodel.cpp
+++ src/source/stadiumtablemodel.cpp
@@ -1,10 +1,22 @@
 #include "header/stadiumtablemodel.h"
 
+/**
+ * @brief StadiumTableModel::StadiumTableModel
+ * A reusable model class for displaying the list of stadiums in a tableView.
+ * @param parent The parent object
+ * @param db The database to read from
+ */
 StadiumTableModel::StadiumTableModel(QObject *parent, Database *db) : QSqlTableModel(parent, *db)
 {
     Initialize();
 }
 
+/**
+ * @brief StadiumTableModel::Initialize
+ * Initialize all of the settings for the stadium table model. This includes
+ * selecting which SQL table to display, setting header values,
+ * and setting the table as uneditable unless manually submitted.
+ */
 void StadiumTableModel::Initialize()
 {
     this->setTable("stadiums");
