7d8fc2cbc2b7fb32474a9c29c729ef7ca5350d1a
ozyx <jessemazzella@gmail.com>
2016-04-16 17:50:43 -0700

    [#7] clicking on stadium details now presents a list of stadiums which are sortable

--- src/header/stadiumdetails.h
+++ src/header/stadiumdetails.h
@@ -14,6 +14,7 @@ class StadiumDetails : public QWidget
 
 public:
     explicit StadiumDetails(QWidget *parent = 0);
+    void initializeStadiumView();
     ~StadiumDetails();
 
 public slots:
--- src/source/stadiumdetails.cpp
+++ src/source/stadiumdetails.cpp
@@ -8,6 +8,34 @@ StadiumDetails::StadiumDetails(QWidget *parent) :
     ui->setupUi(this);
 }
 
+/**
+ * @brief StadiumDetails::initializeStadiumView
+ * Initialize settings for stadium table view.
+ */
+void StadiumDetails::initializeStadiumView()
+{
+    // hide vertical header
+    ui->stadiumDetails_tableView_stadiumInfo->verticalHeader()->setVisible(false);
+
+    // turn alternating row colors on
+    ui->stadiumDetails_tableView_stadiumInfo->setAlternatingRowColors(true);
+
+    // make table uneditable
+    ui->stadiumDetails_tableView_stadiumInfo->setEditTriggers(QTableView::NoEditTriggers);
+
+    // turn word-wrap on... this might do what we want it to do.. I DON'T KNOW
+    ui->stadiumDetails_tableView_stadiumInfo->setWordWrap(true);
+
+    // make it so selection selects each row
+    ui->stadiumDetails_tableView_stadiumInfo->setSelectionBehavior(QAbstractItemView::SelectRows);
+
+    // enable sorting
+    ui->stadiumDetails_tableView_stadiumInfo->setSortingEnabled(true);
+
+    // resize columns to contents
+    ui->stadiumDetails_tableView_stadiumInfo->resizeColumnsToContents();
+}
+
 StadiumDetails::~StadiumDetails()
 {
     delete ui;
@@ -16,4 +44,5 @@ StadiumDetails::~StadiumDetails()
 void StadiumDetails::initializeStadiumTable(StadiumTableModel *stadiumModel)
 {
     ui->stadiumDetails_tableView_stadiumInfo->setModel(stadiumModel);
+    initializeStadiumView();
 }
